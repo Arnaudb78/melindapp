4ef85f6717f742e300526b9de2a2388c4517cfee
Arnaudb78 <arnaudb92230@gmail.com>
2024-09-10 14:05:16 +0200

    fix problems

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="w-full h-[100px] flex flex-col justify-center items-center">
-            <h1 className="text-2xl font-bold">Melind'app</h1>
+            <h1 className="text-2xl font-bold">Melind&apos;app</h1>
         </div>
     );
 }
